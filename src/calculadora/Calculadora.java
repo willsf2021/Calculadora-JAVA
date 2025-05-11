e691f0d903d45db94afa3c30d3ccd77d528a35e3
willsf2021 <willsf2021@gmail.com>
2025-05-11 18:13:16 -0300

    Commit inicial

---
+++ src/calculadora/Calculadora.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculadora;
+
+/**
+ *
+ * @author wills
+ */
+public class Calculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        principal tela = new principal();
+        
+        tela.setVisible(true);
+    }
+    
+}
